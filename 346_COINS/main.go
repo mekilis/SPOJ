d6f7b3bd2d4dab8a9cf7ca89dc0c3f725e9d0979
mekilis <st.nonso@gmail.com>
2018-03-07 23:07:35 +0100

    solved in 0.07s but with 7880M of memory used

--- 346_COINS/main.go
+++ 346_COINS/main.go
@@ -5,13 +5,18 @@
 package main
 
 import (
-	"fmt"
 	"bufio"
+	"fmt"
 	"os"
 	"strconv"
 )
 
+const MAX_SIZE = 1000000000
+
+var lookup = make([]int, MAX_SIZE+1)
+
 func main() {
+	//fmt.Println(lookup)
 	// using less space from now
 	var coin int
 	scanner := bufio.NewScanner(os.Stdin)
@@ -20,15 +25,34 @@ func main() {
 		if scanner.Text() == "" {
 			break
 		}
+		coin, _ = strconv.Atoi(scanner.Text())
+		exchange := compute(coin)
+		//fmt.Println("exchange =", exchange)
 
-		coin,_  = strconv.Atoi(scanner.Text())
-
-		exchange := coin/2 + coin/3 + coin/4
-		fmt.Println("exchange =", exchange)
-		if exchange > coin {
-			coin = exchange
-		}
-
-		fmt.Println(int(coin))
+		fmt.Println(exchange)
 	}
+}
+
+func compute(coin int) int {
+	// base case
+	// max occurs from 12 upwards
+	//fmt.Println("got", coin)
+	if coin < 12 {
+		//fmt.Println("returning", coin)
+		return coin
+	}
+	if lookup[coin] != 0 {
+		//fmt.Println("coin", coin, "has already been process to have", lookup[coin])
+		return lookup[coin]
+	}
+	// compute again
+	//fmt.Println("solving the first time for", coin, "with", coin / 2, coin / 3, coin /4)
+	ex := compute(coin/2) + compute(coin/3) + compute(coin/4)
+	max := coin
+	//fmt.Println(ex, coin)
+	if ex > coin {
+		max = ex
+	}
+	lookup[coin] = max
+	return lookup[coin]
 }
