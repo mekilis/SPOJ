700c15c8dca055d2e19f65257c3113ca394f78c2
mekilis <st.nonso@gmail.com>
2018-03-20 17:00:49 +0100

    still on it

--- 5_PALIN/main.go
+++ 5_PALIN/main.go
@@ -5,8 +5,9 @@
 package main
 
 import (
-	"math/big"
 	"fmt"
+	"math/big"
+	"strings"
 )
 
 func main() {
@@ -17,6 +18,34 @@ func main() {
 	for tt := 0; tt < t; tt++ {
 		fmt.Scanln(K)
 
+		done := false
+
 		fmt.Println("This was entered:", K)
+		KStr := K.String()
+		KArray := strings.Split(KStr, "")
+		// check if it's all 9s
+		l := int64(len(KArray))
+		divisor := new(big.Int)
+		divisor.Set(big.NewInt(l))
+		divisor.Exp(big.NewInt(10), divisor, nil)
+		divisor.Sub(divisor, big.NewInt(1))
+		fmt.Println(divisor)
+
+		if KStr == divisor.String() {
+			fmt.Println("This number is all 9s")
+			divisor.Add(divisor, big.NewInt(2))
+			fmt.Println("next palindrome is", divisor.String())
+			done = true
+		}
+
+		if !done {
+			// check if its already a palindrome
+			mid := l/ 2 + 1
+			left := KArray[:mid]
+			right := KArray[mid:]
+			fmt.Println("This is the breakdown", left, right)
+
+
+		}
 	}
 }
