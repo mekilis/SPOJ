93c1b4c1c045992b46e4caf8abb37ddffc0e37ee
mekilis <st.nonso@gmail.com>
2018-03-28 17:00:16 +0100

    setting up...

---
+++ 302_CANTON/main.go
@@ -0,0 +1,19 @@
+/*
+   Created by Smart Mek on 3/28/18 2:24 PM
+*/
+
+package main
+
+import "fmt"
+
+func main() {
+	var T int
+	var N int64
+
+	fmt.Scanln(&T)
+	for t := 0; t < T; t++ {
+		fmt.Scan(&N)
+
+		//...
+	}
+}
