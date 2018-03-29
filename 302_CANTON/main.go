fbe0b6b21c5986577a0be4c77196a6bbb75a125e
mekilis <st.nonso@gmail.com>
2018-03-29 12:38:00 +0100

    solved in default time

--- 302_CANTON/main.go
+++ 302_CANTON/main.go
@@ -4,16 +4,36 @@
 
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
 func main() {
 	var T int
-	var N int64
+	var n, d, m, top, bot int64
 
 	fmt.Scanln(&T)
 	for t := 0; t < T; t++ {
-		fmt.Scan(&N)
+		fmt.Scan(&n)
+		// n = d(d + 1) / 2
+		// d = (-1 +/- floor(sqrt(1 + 8n))) / 2 ; use only the +ve part
+		d = int64(-1 + math.Floor(math.Sqrt(1 + (8 * float64(n))))) / 2
+		m = n - d * (d + 1) / 2
 
-		//...
+		// top / bot
+		if m < 2 {
+			top = d + m
+			bot = 1
+		} else {
+			top = d - m + 2
+			bot = m
+		}
+
+		if d % 2 != 0 {
+			top, bot = bot, top
+		}
+
+		fmt.Printf("TERM %d IS %d/%d\n", n, top, bot )
 	}
 }
