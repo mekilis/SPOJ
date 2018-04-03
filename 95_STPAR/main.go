eccddbd271f3370f13e3650e10ccfea0db0dc766
mekilis <st.nonso@gmail.com>
2018-04-03 11:10:30 +0100

    solved in 0.00s, 3.0M used

--- 95_STPAR/main.go
+++ 95_STPAR/main.go
@@ -39,16 +39,12 @@ func main() {
 			deque = append(deque, x)
 			dequeLen++
 
-			fmt.Println("appended to deque", deque, dequeLen)
-
 			if dequeLen > 1 {
 				// at least two or more items are in the deque
-				fmt.Println("two or more items are in the deque", deque)
 
 				// compare entry with last item in queue if any
 				if queueLen > 0 {
 					if x < queue[queueLen - 1] {
-						fmt.Println("bad sequence for queue")
 						possible = false
 						continue
 					}
@@ -57,22 +53,18 @@ func main() {
 				// if new entry is greater than the top, remove top
 				for dequeLen > 1 && deque[dequeLen- 1] > deque[dequeLen- 2] {
 
-					fmt.Println(deque[dequeLen - 1], "is greater than", deque[dequeLen - 2], "in", deque)
 					// move lower to queue
 					queue = append(queue, deque[dequeLen- 2])
 					queueLen++
-					fmt.Println("queue now has", queue)
 
 					// update deque
 					deque = append(deque[:dequeLen- 2], deque[dequeLen- 1:]...)
 					dequeLen--
-					fmt.Println("deque is now updated to", deque, "with length", dequeLen)
 				}
 
 				// check deque
 				if dequeLen > 1 {
 					if deque[dequeLen - 1] > deque[dequeLen - 2] {
-						fmt.Println("bad sequence for deque", deque)
 						possible = false
 						continue
 					}
@@ -81,15 +73,11 @@ func main() {
 
 			// check queue
 			if queueLen > 1 {
-				fmt.Println("two or more items in the queue", queue, "now comparing", queue[queueLen - 1], "with", queue[queueLen - 2])
 				if queue[queueLen - 1] < queue[queueLen - 2] {
 					// invalid order
-					fmt.Println("invalid queue sequence... breaking")
 					possible = false
 					continue
 				}
-
-				fmt.Println("sequence is valid for queue", queue)
 			}
 		}
 
@@ -103,8 +91,6 @@ func main() {
 		queue = make([]int, 0)
 		dequeLen = 0
 		queueLen = 0
-
-		fmt.Println("variables used", x, N, dequeLen, queueLen, deque, queue, possible)
 
 		fmt.Scan(&N)
 	}
