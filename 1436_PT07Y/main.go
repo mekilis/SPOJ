eb3e896084008a16fdc815ebf2ed5eef17464868
mekilis <st.nonso@gmail.com>
2018-04-04 15:09:47 +0100

    solved in 0.09s, 4.0M used

--- 1436_PT07Y/main.go
+++ 1436_PT07Y/main.go
@@ -8,7 +8,6 @@ import "fmt"
 
 var adj [][]int
 var visited []bool
-var recursion []bool
 
 func main() {
 	var N, M, u, v int
@@ -17,9 +16,9 @@ func main() {
 	fmt.Scan(&N)
 	fmt.Scan(&M)
 
+	// one-indexed
 	adj = make([][]int, N + 1)
 	visited = make([]bool, N + 1)
-	recursion = make([]bool, N + 1)
 
 	for m := 0; m < M; m++ {
 		fmt.Scan(&u)
@@ -29,50 +28,59 @@ func main() {
 		adj[v] = append(adj[v], u)
 	}
 
-	fmt.Println("before", adj, visited)
-
-	// iterate through the nodes
-	for n := 1; n <= N; n++ {
-		fmt.Println(n, visited)
-		if isCyclic(n) {
-			cyclic = true
-			break
-		}
+	// check if it's possible
+	if M != N - 1 || N == 1 {
+		cyclic = true
+		goto fin
 	}
 
-	fmt.Println("after", adj, visited, cyclic)
+	///fmt.Println("before", adj, visited)
 
-	if cyclic {
-		fmt.Println("graph is cyclic")
-	} else {
-		fmt.Println("graph isn't cyclic")
-	}
-}
-
-// redundant part
-func dfs(n int) {
-	visited[n] = true
-
-	for i := 0; i < len(adj[n]); i++ {
-		w := adj[n][i]
-
-		if !visited[w] {
-			dfs(w)
-		}
-	}
-}
-
-func isCyclic(n int) bool {
-	if !visited[n] {
-		visited[n] = true
-		recursion[n] = true
-
-		for i := 0; i < len(adj[n]); i++ {
-			if !visited[adj[n][i]] && isCyclic(adj[n][i]) || recursion[adj[n][i]]{
-				return true
+	// reuse u and v
+	for u = 1; u <= N; u++ {
+		if !visited[u] {
+			if isCyclic(u, -1) {
+				cyclic = true
+				break
 			}
 		}
 	}
-	recursion[n] = false
+
+	//fmt.Println("after", adj, visited)
+	fin:
+		if cyclic {
+			fmt.Println("NO")
+		} else {
+			fmt.Println("YES")
+		}
+}
+
+func isCyclic(v int, parent int) bool {
+
+	// mark this node
+	//fmt.Println("marking node", v, "with parent", parent)
+	visited[v] = true
+
+	// iterate through all adjacent nodes
+	for i := 0; i < len(adj[v]); i++ {
+
+		//fmt.Println("working on edge", v, ",", adj[v][i])
+		// check if node has been visited, if not, dig deeper
+		if !visited[adj[v][i]] {
+			if isCyclic(adj[v][i], v) {
+				//fmt.Println("a cycle for", v)
+				return true
+			}
+			// check if node is not same as parent
+		} else if adj[v][i] != parent {
+			//fmt.Println("node", adj[v][i], "has been visited and is not a parent. a cycle exists")
+			return true
+		}
+
+		//fmt.Println("node", adj[v][i], "has already been visited and is a parent")
+
+	}
+
+	//fmt.Println("no cycle for", v)
 	return false
 }
