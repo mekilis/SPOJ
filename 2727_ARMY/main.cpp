78da0facfcb891426d249d6402d692175964b905
mekilis <st.nonso@gmail.com>
2018-04-16 12:31:20 +0100

    solved army in 0.05s, 3.2M used

--- 2727_ARMY/main.cpp
+++ 2727_ARMY/main.cpp
@@ -1,35 +1,50 @@
 #include <iostream>
+#include <stdio.h>
+
 using namespace std;
 
 int main() {
-    int T, NG, NM, x;
-    int NGMax = 0;
-    int NMMax = 0;
-    cin >> T;
+    int T, NG, NM;
+    int NGMax = -1;
+    int NMMax = -1;
+    scanf("%d",&T);
 
     for (int t = 0; t < T; t++) {
-        cin.get(); //trash
-        cin >> NG;
-        cin >> NM;
+        scanf("%d",&NG);
+        scanf("%d",&NM);
+
+        int NGArr[NG];
+        int NMArr[NM];
 
         for (int ng = 0; ng < NG; ng++) {
-            cin >> x;
-            if (x > NGMax) {
-                NGMax = x;
-            }
+            scanf("%d",&NGArr[ng]);
         }
 
         for (int nm = 0; nm < NM; nm++) {
-            cin >> x;
-            if (x > NMMax) {
-                NMMax = x;
+            scanf("%d",&NMArr[nm]);
+        }
+
+        NGMax = NGArr[0];
+        NMMax = NMArr[0];
+
+        for (int i = 1; i < NG; i++) {
+            if (NGMax < NGArr[i]) {
+                NGMax = NGArr[i];
             }
         }
 
-        if (NMMax > NGMax) {
-            cout << "MechaGodzilla" << endl;
+        for (int i = 1; i < NM; i++) {
+            if (NMMax < NMArr[i]) {
+                NMMax = NMArr[i];
+            }
+        }
+
+        if (NGMax >= NMMax) {
+            printf("Godzilla\n");
+        } else if (NGMax < NMMax) {
+            printf("MechaGodzilla\n");
         } else {
-            cout << "Godzilla" << endl;
+            printf("uncertain\n");
         }
     }
 
