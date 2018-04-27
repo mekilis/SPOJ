d05537a7376c0d1ab268b011660366b3a8d1c696
mekilis <st.nonso@gmail.com>
2018-04-27 18:14:15 +0100

    setting up tricount

---
+++ 1724_TRICOUNT/main.go
@@ -0,0 +1,17 @@
+/*
+   Created by Smart Mek on Apr 27, 2018 5:54 PM
+*/
+
+package main
+
+import "fmt"
+
+func main() {
+	var T, n int
+
+	fmt.Scan(&T)
+
+	for t := 0; t < T; t++ {
+		fmt.Scan(&n)
+	}
+}
