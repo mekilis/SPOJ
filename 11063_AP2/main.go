ee6971a1f18fb96b949a888a64240d7c9254aadd
mekilis <st.nonso@gmail.com>
2018-05-05 13:54:57 +0100

    solved in 0.04s 4M used after 7 TLEs

--- 11063_AP2/main.go
+++ 11063_AP2/main.go
@@ -6,49 +6,41 @@ package main
 
 import (
 	"fmt"
-	"math/big"
 )
 
 func main() {
 	//var threeTerm, threeLastTerm, sum int64 = 3, 8, 55
-	//var threeTerm, threeLastTerm, sum int64 = 10, 55, 650 // using big.Int instead
-	var threeTerm, threeLastTerm, sum = big.NewInt(144115188075856304), big.NewInt(144115188075855890), big.NewInt(7349874591868660947)
+	var threeTerm, threeLastTerm, sum int64 = 10, 55, 650
 	var T = 1
 
 	fmt.Scan(&T)
 	for t := 0; t < T; t++ {
-		fmt.Scan(threeTerm)
-		fmt.Scan(threeLastTerm)
-		fmt.Scan(sum)
+		fmt.Scan(&threeTerm)
+		fmt.Scan(&threeLastTerm)
+		fmt.Scan(&sum)
 
-		var a, d, n, l, opRight, opLeft = new(big.Int), new(big.Int), new(big.Int), new(big.Int), new(big.Int), new(big.Int)
+		var a, d, n int64
+
 		// Formula: Sn = (n/2)(a + l) == (n/2)(2a + (n - 1)d)
 		// But a + l = 3term + 3lastterm, l = 3t + 3lt - a
 		// Therefore, Sn = (n/2)(3t + 3lt)
-		opRight.Add(threeTerm, threeLastTerm)
-		opLeft.Mul(big.NewInt(2), sum)
+		n = (2 * sum) / (threeTerm + threeLastTerm)
 
-		//n = (2 * sum) / (threeTerm + threeLastTerm)
-		n.Div(opLeft, opRight)
 		fmt.Println(n)
 
-		// 3t = a + 2d -- i, 3lt = a + (n - 1 -2)d = a + (n - 3)d
-		// Using elimination ==> d = (3t - 3lt) / (5 - n), Since n >= 7, division by zero can never occur
-		// d = (threeTerm - threeLastTerm) / (5 - n)
-		opRight.Sub(big.NewInt(5), n)
-		opLeft.Sub(threeTerm, threeLastTerm)
-		d.Div(opLeft, opRight)
+		// 3t = a + 2d -- i
+		// 3lt = a + (n - 1 -2)d = a + (n - 3)d
+		// Using elimination ==> d = (3t - 3lt) / (5 - n)
+		// Since n >= 7, division by zero can never occur
+		d = (threeTerm - threeLastTerm) / (5 - n)
 
-		//a = threeTerm - 2*d
-		opRight.Mul(big.NewInt(2), d)
-		a.Sub(threeTerm, opRight)
+		a = threeTerm - 2*d
+		//fmt.Println(d)
+		// no need to compute l
 
-		//l := threeTerm + threeLastTerm - a
-		l.Add(threeTerm, threeLastTerm)
-		l.Sub(l, a)
-
-		for i := a; i.Cmp(l) <= 0; i.Add(i, d) {
-			fmt.Print(i, " ")
+		for i := int64(0); i < n; i++ {
+			fmt.Print(a, " ")
+			a += d
 		}
 
 		fmt.Println()
