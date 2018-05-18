0446ad0e6b7949404b2fb31858bae27722d1281b
mekilis <st.nonso@gmail.com>
2018-05-18 22:10:45 +0100

    golang not available in list. would use C++ for this working solution

--- 3923_BYTESM2/main.go
+++ 3923_BYTESM2/main.go
@@ -5,55 +5,49 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
-	"os"
-	"strconv"
-	"strings"
-
-	"log"
 )
 
 func main() {
+	//start := time.Now()
 	var T, h, w, m int
-	var line []string
+	//var line []string
 	tiles := make([][]int, 0)
 	tilesCopy := make([][]int, 0)
 
-	file, err := os.Open("SPOJ/3923_BYTESM2/data.txt")
-	if err != nil {
-		log.Fatal(err)
-	}
+	//file, err := os.Open("SPOJ/3923_BYTESM2/bigdata.txt")
+	//if err != nil {
+	//	log.Fatal(err)
+	//}
 
-	defer file.Close()
+	//defer file.Close()
 
-	scanner := bufio.NewScanner(file)
-	if scanner.Scan() {
-		T, _ = strconv.Atoi(scanner.Text())
-	}
+	//scanner := bufio.NewScanner(file)
+	//if scanner.Scan() {
+	//	T, _ = strconv.Atoi(scanner.Text())
+	//}
 
-	//fmt.Scan(&T)
+	fmt.Scan(&T)
 	for t := 0; t < T; t++ {
-		//fmt.Scan(&h)
-		//fmt.Scan(&w)
+		fmt.Scan(&h)
+		fmt.Scan(&w)
 
-		if scanner.Scan() {
-			line = strings.Split(scanner.Text(), " ")
-			h, _ = strconv.Atoi(line[0])
-			w, _ = strconv.Atoi(line[1])
-		}
-
+		//if scanner.Scan() {
+		//	line = strings.Split(scanner.Text(), " ")
+		//	h, _ = strconv.Atoi(line[0])
+		//	w, _ = strconv.Atoi(line[1])
+		//}
 
 		tiles = make([][]int, h)
 		tilesCopy = make([][]int, h)
 
 		for i := 0; i < h; i++ {
-			if scanner.Scan() {
-				line = strings.Split(scanner.Text(), " ")
-			}
+			//if scanner.Scan() {
+			//	line = strings.Split(scanner.Text(), " ")
+			//}
 			for j := 0; j < w; j++ {
-				//fmt.Scan(&m)
-				m, _ = strconv.Atoi(line[j])
+				fmt.Scan(&m)
+				//m, _ = strconv.Atoi(line[j])
 				tiles[i] = append(tiles[i], m)
 				tilesCopy[i] = append(tilesCopy[i], m)
 			}
@@ -61,37 +55,20 @@ func main() {
 
 		max := 0
 		var left, right, down int
-		for i := 0; i < h - 1; i++ {
+		for i := 0; i < h-1; i++ {
 			for j := 0; j < w; j++ {
 
-				//// downwards
-				//temp := tiles[i][j] + tiles[i+1][j]
-				//if temp > max {
-				//	max = temp
-				//}
-				//
-				//// left diagonal
-				//if j > 0 {
-				//	temp = tiles[i][j] + tiles[i+1][j-1]
-				//	if temp > max {
-				//		max = temp
-				//	}
-				//}
-				//
-				//// check right diagonal
-				//if j < w - 1 {
-				//	temp = tiles[i][j] + tiles[i+1][j+1]
-				//	if temp > max {
-				//		max = temp
-				//	}
-				//}
+				// constant direction
+				down = tilesCopy[i][j] + tilesCopy[i+1][j]
+				if down > tiles[i+1][j] {
+					tiles[i+1][j] = down
+				}
 
-				if j > 0 && j < w -1 {
+				if j > 0 && j < w-1 {
 					// in between
 					//fmt.Println("tilesCopy", tilesCopy)
 					left = tilesCopy[i][j] + tilesCopy[i+1][j-1]
 					right = tilesCopy[i][j] + tilesCopy[i+1][j+1]
-					down = tilesCopy[i][j] + tilesCopy[i+1][j]
 
 					// check
 					if left > tiles[i+1][j-1] {
@@ -100,38 +77,36 @@ func main() {
 					if right > tiles[i+1][j+1] {
 						tiles[i+1][j+1] = right
 					}
-					if down > tiles[i+1][j] {
-						tiles[i+1][j] = down
-					}
+
 				} else if j == 0 {
+					// leftmost edge
 					right = tilesCopy[i][j] + tilesCopy[i+1][j+1]
-					down = tilesCopy[i][j] + tilesCopy[i+1][j]
 
 					if right > tiles[i+1][j+1] {
 						tiles[i+1][j+1] = right
 					}
-					if down > tiles[i+1][j] {
-						tiles[i+1][j] = down
-					}
 
 				} else {
 					left = tilesCopy[i][j] + tilesCopy[i+1][j-1]
-					down = tilesCopy[i][j] + tilesCopy[i+1][j]
-
 					if left > tiles[i+1][j-1] {
 						tiles[i+1][j-1] = left
 					}
-
-					if down > tiles[i+1][j] {
-						tiles[i+1][j] = down
-					}
 				}
 			}
-			break
+			// update tilesCopy row
+			tilesCopy[i+1] = tiles[i+1] // this works :)
+			//break
 		}
 
-		fmt.Println("max:", max, tiles)
+		//fmt.Println("max:", max, tiles)
 		//fmt.Println(tilesCopy)
+		for i := 0; i < w; i++ {
+			if tiles[h-1][i] > max {
+				max = tiles[h-1][i]
+			}
+		}
+		fmt.Println(max)
+		//fmt.Println("time taken", time.Since(start).Seconds())
 
 	}
 
