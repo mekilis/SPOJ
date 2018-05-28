ecf23286af67f3105803c3e867faaa4dd6c66341
mekilis <st.nonso@gmail.com>
2018-05-28 10:38:44 +0100

    TLE even with this. switching to C++

--- 1043_GSS1/main.go
+++ 1043_GSS1/main.go
@@ -7,10 +7,12 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"math"
 	"os"
 )
 
 func main() {
+
 	var N, M, n, m, x, i, j int64
 	var list = make([]int64, 0)
 
@@ -43,6 +45,17 @@ func main() {
 		list = append(list, x)
 	}
 
+	// Method 2: Set up tree
+	c := math.Ceil(math.Log2(float64(N)))
+	size := 2*int(math.Pow(2, c)) - 1
+
+	tree = make([]Node, 0)
+	for i := 0; i < size; i++ {
+		tree = append(tree, NewNode())
+	}
+
+	constructTree(tree, list, 0, n-1, 1)
+
 	scanner.Scan()
 	b = scanner.Bytes()
 	for len(b) == 0 {
@@ -73,7 +86,12 @@ func main() {
 		j = toInt(b)
 
 		//fmt.Println("M", i, j, list)
-		_, _, max := findMaximumSubarray(list, i-1, j-1)
+		// 1. Method 1
+		//_, _, max := findMaximumSubarray(list, i-1, j-1)
+
+		// 2. Method 2
+		max := segmentTreeMax(tree, n, i-1, j-1)
+
 		fmt.Println(max)
 	}
 }
@@ -98,6 +116,95 @@ func toInt(buf []byte) int64 {
 	return n
 }
 
+// using another O(n log (n)) solution - segment trees
+type Node struct {
+	maxPrefixSum   int64
+	maxSuffixSum   int64
+	maxSubArraySum int64
+	totalSum       int64
+}
+
+var mathMin = int64(math.MinInt16)
+
+func NewNode() Node {
+	n := Node{maxPrefixSum: mathMin, maxSuffixSum: mathMin, maxSubArraySum: mathMin, totalSum: 0}
+	return n
+}
+
+var tree []Node
+
+func segmentTreeMax(tree []Node, n, i, j int64) int64 {
+	node := query(tree, 0, n-1, i, j, 1)
+
+	// fmt.Printf("node: %+v\n", node)
+
+	return node.maxSubArraySum
+}
+
+func merge(leftChild, rightChild Node) Node {
+
+	var parentNode = NewNode()
+
+	parentNode.maxPrefixSum = max(leftChild.maxPrefixSum, leftChild.totalSum+
+		rightChild.maxPrefixSum)
+
+	parentNode.maxSuffixSum = max(rightChild.maxSuffixSum, rightChild.totalSum+
+		leftChild.maxSuffixSum)
+
+	parentNode.totalSum = leftChild.totalSum + rightChild.totalSum
+
+	parentNode.maxSubArraySum = max(max(leftChild.maxSubArraySum,
+		rightChild.maxSubArraySum), leftChild.maxSuffixSum+rightChild.maxPrefixSum)
+
+	return parentNode
+}
+
+func max(x int64, y int64) (z int64) {
+	z = x
+	if y > x {
+		z = y
+	}
+	return
+}
+
+func constructTree(tree []Node, A []int64, start, end, i int64) {
+	if start == end {
+		tree[i].totalSum = A[start]
+		tree[i].maxSuffixSum = A[start]
+		tree[i].maxPrefixSum = A[start]
+		tree[i].maxSubArraySum = A[start]
+
+		return
+	}
+
+	// build children recursively
+	mid := (start + end) / 2
+	constructTree(tree, A, start, mid, 2*i)
+	constructTree(tree, A, mid+1, end, 2*i+1)
+
+	// merge
+	tree[i] = merge(tree[2*i], tree[2*i+1])
+}
+
+func query(tree []Node, ss, se, qs, qe, i int64) Node {
+	if ss > qe || se < qs {
+		return NewNode()
+	}
+
+	if ss >= qs && se <= qe {
+		return tree[i]
+	}
+
+	mid := (ss + se) / 2
+
+	left, right := query(tree, ss, mid, qs, qe, 2*i),
+		query(tree, mid+1, se, qs, qe, 2*i+1)
+
+	return merge(left, right)
+}
+
+/*
+// O(n log (n)) solution still timed out
 func findMaximumSubarray(A []int64, low, high int64) (int64, int64, int64) {
 	if high == low {
 		return low, high, A[low]
@@ -109,7 +216,7 @@ func findMaximumSubarray(A []int64, low, high int64) (int64, int64, int64) {
 
 		mid := (low + high) / 2
 		leftLow, leftHigh, leftSum = findMaximumSubarray(A, low, mid)
-		rightLow, righttHigh, rightSum = findMaximumSubarray(A, mid + 1, high)
+		rightLow, righttHigh, rightSum = findMaximumSubarray(A, mid+1, high)
 		crossLow, crossHigh, crossSum = findMaximumCrossingSubarray(A, low, mid, high)
 
 		if leftSum >= rightSum && leftSum >= crossSum {
@@ -138,7 +245,7 @@ func findMaximumCrossingSubarray(A []int64, low, mid, high int64) (int64, int64,
 
 	rightSum = -1 << 63
 	sum = 0
-	for j := mid+1; j <= high; j++ {
+	for j := mid + 1; j <= high; j++ {
 		sum += A[j]
 		if sum > rightSum {
 			rightSum = sum
@@ -148,3 +255,4 @@ func findMaximumCrossingSubarray(A []int64, low, mid, high int64) (int64, int64,
 
 	return maxLeft, maxRight, leftSum + rightSum
 }
+*/
